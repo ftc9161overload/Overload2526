c62f3af2bb71d6ff63f99d91f9af8436ba1f4dc4
AsherChilli <115118008+AsherChilli@users.noreply.github.com>
2025-10-04 11:50:58 -0500

    Added Util Stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Poses.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.Util;
+
+import com.pedropathing.geometry.Pose;
+
+
+//Make your poses using THIS website: https://visualizer.pedropathing.com
+
+//All tutorials and explanations can be found HERE: https://pedropathing.com
+
+public class Poses {
+
+
+    public static Pose start = new Pose(0, 0, 0);
+
+    public static Pose blueGoal = new Pose(16, 132);
+
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/UniConstants.java
@@ -0,0 +1,102 @@
+package org.firstinspires.ftc.teamcode.Util;
+
+import android.graphics.Color;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.firstinspires.ftc.vision.opencv.ColorRange;
+import org.firstinspires.ftc.vision.opencv.ImageRegion;
+
+@Config
+public class UniConstants {
+
+
+
+    //Drive
+    public static final String DRIVE_FRONT_LEFT_STRING = "LFM";
+    public static final String DRIVE_FRONT_RIGHT_STRING = "RFM";
+    public static final String DRIVE_BACK_LEFT_STRING = "LRM";
+    public static final String DRIVE_BACK_RIGHT_STRING = "RRM";
+    public static final DcMotorEx.Direction DRIVE_FRONT_LEFT_DIRECTION = DcMotorEx.Direction.FORWARD;
+    public static final DcMotorEx.Direction DRIVE_FRONT_RIGHT_DIRECTION = DcMotorEx.Direction.REVERSE;
+    public static final DcMotorEx.Direction DRIVE_BACK_LEFT_DIRECTION = DcMotorEx.Direction.FORWARD;
+    public static final DcMotorEx.Direction DRIVE_BACK_RIGHT_DIRECTION = DcMotorEx.Direction.REVERSE;
+
+    public enum loggingState{
+        DISABLED,
+        ENABLED,
+        EXTREME
+    }
+
+    public enum slotState{
+        PURPLE,
+        GREEN,
+        EMPTY
+    }
+
+    public static final String COLOR_SENSOR_SLOT_1_STRING = "COLOR0";
+    public static final String COLOR_SENSOR_SLOT_2_STRING = "COLOR1";
+    public static final String COLOR_SENSOR_SLOT_3_STRING = "COLOR2";
+
+    public static final String ACTIVE_INTAKE_STRING = "ACTIVE";
+    public static final String LAUNCHER_STRING  = "LAUNCHER";
+    public static final String ROTARY_STRING = "ROTARY";
+    public static final DcMotorSimple.Direction ROTARY_DIRECTION = DcMotorSimple.Direction.FORWARD; //TODO: Ensure this is correct direction
+
+
+    public static int PURPLE_ARTIFACT_UPPER_HUE = 350;
+    public static int PURPLE_ARTIFACT_LOWER_HUE = 275;
+
+    public static int GREEN_ARTIFACT_UPPER_HUE = 150;
+    public static int GREEN_ARTIFACT_LOWER_HUE = 100;
+
+    public static final int SPACE_BETWEEN_ROTARY_SLOTS = 300;
+
+
+    public static final AprilTagProcessor aprilTagProcessor = new AprilTagProcessor.Builder()
+            .setDrawTagID(true)
+            .setDrawAxes(true)
+            .setDrawCubeProjection(true)
+            .build();
+    public static final ColorBlobLocatorProcessor colorLocatorPurple = new ColorBlobLocatorProcessor.Builder()
+            .setTargetColorRange(ColorRange.ARTIFACT_PURPLE)   // Use a predefined color match
+            .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
+            .setRoi(ImageRegion.asUnityCenterCoordinates(-0.75, 0.75, 0.75, -0.75))
+            .setDrawContours(true)   // Show contours on the Stream Preview
+            .setBoxFitColor(0)       // Disable the drawing of rectangles
+            .setCircleFitColor(Color.rgb(255, 255, 0)) // Draw a circle
+            .setBlurSize(5)          // Smooth the transitions between different colors in image
+
+            // the following options have been added to fill in perimeter holes.
+            .setDilateSize(15)       // Expand blobs to fill any divots on the edges
+            .setErodeSize(15)        // Shrink blobs back to original size
+            .setMorphOperationType(ColorBlobLocatorProcessor.MorphOperationType.CLOSING)
+
+            .build();
+
+    public static final ColorBlobLocatorProcessor colorLocatorGreen = new ColorBlobLocatorProcessor.Builder()
+            .setTargetColorRange(ColorRange.ARTIFACT_GREEN)   // Use a predefined color match
+            .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
+            .setRoi(ImageRegion.asUnityCenterCoordinates(-0.75, 0.75, 0.75, -0.75))
+            .setDrawContours(true)   // Show contours on the Stream Preview
+            .setBoxFitColor(0)       // Disable the drawing of rectangles
+            .setCircleFitColor(Color.rgb(255, 255, 0)) // Draw a circle
+            .setBlurSize(5)          // Smooth the transitions between different colors in image
+
+            // the following options have been added to fill in perimeter holes.
+            .setDilateSize(15)       // Expand blobs to fill any divots on the edges
+            .setErodeSize(15)        // Shrink blobs back to original size
+            .setMorphOperationType(ColorBlobLocatorProcessor.MorphOperationType.CLOSING)
+
+            .build();
+
+
+
+
+
+
+}
