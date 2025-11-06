2b58d06adf330ae961c520dd8f2f5dbb62327768
Username-not-used <188395073+Username-not-used@users.noreply.github.com>
2025-11-05 22:26:22 -0600

    Create Coordinates.java
    Created coordinates class and added some filler values for hopefully Natalia to add to

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Coordinates.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+// Basic class to build off of
+public class Coordinates {
+    // Filler values
+    public int[] redGoal = {122, 122};
+    public int[] redFrontScore = {100, 100};
+    public int[] redBackScore = {};
+    public int[] blueGoal = {};
+    public int[] blueFrontScore = {};
+}
