d28dd1fb9ae537029e4c10ae896a30f5038330fd
AsherChilli <115118008+AsherChilli@users.noreply.github.com>
2025-11-10 00:31:02 -0600

    wrote a bunch of uniconstant and teleop base

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NextFTCTeleOp.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.SwerveDrivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.IntakeSubystem;
+import org.firstinspires.ftc.teamcode.subsystems.OuttakeSubystem;
+import org.firstinspires.ftc.teamcode.subsystems.RotarySubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.UniConstants;
+
+import dev.nextftc.ftc.NextFTCOpMode;
+import kotlin.Unit;
+
+@TeleOp(name = "NextFTC TeleOp", group = "TeleOp")
+@Configurable
+public class NextFTCTeleOp extends NextFTCOpMode {
+
+
+    private static RotarySubsystem rotarySubsystem;
+    private static IntakeSubystem intakeSubystem;
+    private static OuttakeSubystem outtakeSubystem;
+    private static SwerveDrivetrain swerveDrivetrain;
+
+    @Override
+    public void onInit() {
+        rotarySubsystem = new RotarySubsystem(hardwareMap, UniConstants.ROTARY_MOTOR_STRING);
+        intakeSubystem = new IntakeSubystem(UniConstants.INTAKE_MOTOR_STRING, hardwareMap);
+        outtakeSubystem = new OuttakeSubystem(UniConstants.OUTTAKE_MOTOR_STRING, hardwareMap);
+        swerveDrivetrain = new SwerveDrivetrain(hardwareMap);
+    }
+
+    @Override
+    public void onWaitForStart() {
+
+    }
+
+    @Override
+    public void onStartButtonPressed() {
+
+    }
+
+    @Override
+    public void onUpdate() {
+
+
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/UniConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/UniConstants.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import android.graphics.Color;
 
 //import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
@@ -17,14 +18,20 @@ public class UniConstants {
 
 
     //Drive
-    public static final String DRIVE_FRONT_LEFT_STRING = "LFM";
-    public static final String DRIVE_FRONT_RIGHT_STRING = "RFM";
-    public static final String DRIVE_BACK_LEFT_STRING = "LRM";
-    public static final String DRIVE_BACK_RIGHT_STRING = "RRM";
+    public static final String DRIVE_FRONT_LEFT_STRING = "LFM", DRIVE_FRONT_RIGHT_STRING = "RFM", DRIVE_BACK_LEFT_STRING = "LRM", DRIVE_BACK_RIGHT_STRING = "RRM";
+    public static final String DRIVE_FRONT_LEFT_SERVO_STRING = "LFS", DRIVE_FRONT_RIGHT_SERVO_STRING = "RFS", DRIVE_BACK_LEFT_SERVO_STRING = "LRS", DRIVE_BACK_RIGHT_SERVO_STRING = "RRS";
+    public static final String DRIVE_FRONT_LEFT_ANALOG_INPUT = "LFA", DRIVE_FRONT_RIGHT_ANALOG_INPUT = "RFA", DRIVE_BACK_LEFT_ANALOG_INPUT = "LRA", DRIVE_BACK_RIGHT_ANALOG_INPUT = "RRA";
     public static final DcMotorEx.Direction DRIVE_FRONT_LEFT_DIRECTION = DcMotorEx.Direction.FORWARD;
     public static final DcMotorEx.Direction DRIVE_FRONT_RIGHT_DIRECTION = DcMotorEx.Direction.REVERSE;
     public static final DcMotorEx.Direction DRIVE_BACK_LEFT_DIRECTION = DcMotorEx.Direction.FORWARD;
     public static final DcMotorEx.Direction DRIVE_BACK_RIGHT_DIRECTION = DcMotorEx.Direction.REVERSE;
+
+
+    public static final String ROTARY_MOTOR_STRING = "RoM", OUTTAKE_MOTOR_STRING = "OuM", INTAKE_MOTOR_STRING = "InM";
+
+    public static final  DcMotorEx.ZeroPowerBehavior ROTARY_ZERO_BEHAVIOR = DcMotorEx.ZeroPowerBehavior.BRAKE, OUTTAKE_ZERO_BEHAVIOR = DcMotorEx.ZeroPowerBehavior.FLOAT, INTAKE_ZERO_BEHAVIOR = DcMotorEx.ZeroPowerBehavior.BRAKE;
+    public static final DcMotorEx.RunMode ROTARY_RUN_MODE = DcMotorEx.RunMode.RUN_USING_ENCODER, OUTTAKE_RUN_MODE = DcMotorEx.RunMode.RUN_USING_ENCODER;
+    public static final DcMotorEx.Direction ROTARY_DIRECTION = DcMotorEx.Direction.FORWARD, OUTTAKE_DIRECTION = DcMotorEx.Direction.FORWARD, INTAKE_DIRECTION = DcMotorEx.Direction.FORWARD;
 
     public enum loggingState{
         DISABLED,
@@ -42,10 +49,7 @@ public class UniConstants {
     public static final String COLOR_SENSOR_SLOT_2_STRING = "COLOR1";
     public static final String COLOR_SENSOR_SLOT_3_STRING = "COLOR2";
 
-    public static final String ACTIVE_INTAKE_STRING = "ACTIVE";
-    public static final String LAUNCHER_STRING  = "LAUNCHER";
-    public static final String ROTARY_STRING = "ROTARY";
-    public static final DcMotorSimple.Direction ROTARY_DIRECTION = DcMotorSimple.Direction.FORWARD; //TODO: Ensure this is correct direction
+
 
 
     public static int PURPLE_ARTIFACT_UPPER_HUE = 350;
@@ -57,46 +61,46 @@ public class UniConstants {
     public static final int SPACE_BETWEEN_ROTARY_SLOTS = 300;
 
 
-    public static final AprilTagProcessor aprilTagProcessor = new AprilTagProcessor.Builder()
-            .setDrawTagID(true)
-            .setDrawAxes(true)
-            .setDrawCubeProjection(true)
-            .build();
-    public static final ColorBlobLocatorProcessor colorLocatorPurple = new ColorBlobLocatorProcessor.Builder()
-            .setTargetColorRange(ColorRange.ARTIFACT_PURPLE)   // Use a predefined color match
-            .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
-            .setRoi(ImageRegion.asUnityCenterCoordinates(-0.75, 0.75, 0.75, -0.75))
-            .setDrawContours(true)   // Show contours on the Stream Preview
-            .setBoxFitColor(0)       // Disable the drawing of rectangles
-            .setCircleFitColor(Color.rgb(255, 255, 0)) // Draw a circle
-            .setBlurSize(5)          // Smooth the transitions between different colors in image
-
-            // the following options have been added to fill in perimeter holes.
-            .setDilateSize(15)       // Expand blobs to fill any divots on the edges
-            .setErodeSize(15)        // Shrink blobs back to original size
-            .setMorphOperationType(ColorBlobLocatorProcessor.MorphOperationType.CLOSING)
-
-            .build();
-
-    public static final ColorBlobLocatorProcessor colorLocatorGreen = new ColorBlobLocatorProcessor.Builder()
-            .setTargetColorRange(ColorRange.ARTIFACT_GREEN)   // Use a predefined color match
-            .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
-            .setRoi(ImageRegion.asUnityCenterCoordinates(-0.75, 0.75, 0.75, -0.75))
-            .setDrawContours(true)   // Show contours on the Stream Preview
-            .setBoxFitColor(0)       // Disable the drawing of rectangles
-            .setCircleFitColor(Color.rgb(255, 255, 0)) // Draw a circle
-            .setBlurSize(5)          // Smooth the transitions between different colors in image
-
-            // the following options have been added to fill in perimeter holes.
-            .setDilateSize(15)       // Expand blobs to fill any divots on the edges
-            .setErodeSize(15)        // Shrink blobs back to original size
-            .setMorphOperationType(ColorBlobLocatorProcessor.MorphOperationType.CLOSING)
-
-            .build();
-
-
-
-
+//    public static final AprilTagProcessor aprilTagProcessor = new AprilTagProcessor.Builder()
+//            .setDrawTagID(true)
+//            .setDrawAxes(true)
+//            .setDrawCubeProjection(true)
+//            .build();
+//    public static final ColorBlobLocatorProcessor colorLocatorPurple = new ColorBlobLocatorProcessor.Builder()
+//            .setTargetColorRange(ColorRange.ARTIFACT_PURPLE)   // Use a predefined color match
+//            .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
+//            .setRoi(ImageRegion.asUnityCenterCoordinates(-0.75, 0.75, 0.75, -0.75))
+//            .setDrawContours(true)   // Show contours on the Stream Preview
+//            .setBoxFitColor(0)       // Disable the drawing of rectangles
+//            .setCircleFitColor(Color.rgb(255, 255, 0)) // Draw a circle
+//            .setBlurSize(5)          // Smooth the transitions between different colors in image
+//
+//            // the following options have been added to fill in perimeter holes.
+//            .setDilateSize(15)       // Expand blobs to fill any divots on the edges
+//            .setErodeSize(15)        // Shrink blobs back to original size
+//            .setMorphOperationType(ColorBlobLocatorProcessor.MorphOperationType.CLOSING)
+//
+//            .build();
+//
+//    public static final ColorBlobLocatorProcessor colorLocatorGreen = new ColorBlobLocatorProcessor.Builder()
+//            .setTargetColorRange(ColorRange.ARTIFACT_GREEN)   // Use a predefined color match
+//            .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
+//            .setRoi(ImageRegion.asUnityCenterCoordinates(-0.75, 0.75, 0.75, -0.75))
+//            .setDrawContours(true)   // Show contours on the Stream Preview
+//            .setBoxFitColor(0)       // Disable the drawing of rectangles
+//            .setCircleFitColor(Color.rgb(255, 255, 0)) // Draw a circle
+//            .setBlurSize(5)          // Smooth the transitions between different colors in image
+//
+//            // the following options have been added to fill in perimeter holes.
+//            .setDilateSize(15)       // Expand blobs to fill any divots on the edges
+//            .setErodeSize(15)        // Shrink blobs back to original size
+//            .setMorphOperationType(ColorBlobLocatorProcessor.MorphOperationType.CLOSING)
+//
+//            .build();
+//
+//
+//
+//
 
 
 }
