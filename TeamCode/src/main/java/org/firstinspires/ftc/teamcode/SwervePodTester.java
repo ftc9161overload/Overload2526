feb0ea9770ebad98c57f3952e81e8ded6f48a664
AsherChilli <asherchildress@gmail.com>
2025-11-10 16:15:38 -0600

    set podTester to be configurable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwervePodTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwervePodTester.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -7,17 +8,21 @@ import org.firstinspires.ftc.teamcode.subsystems.SwervePodSubsystem;
 
 
 @TeleOp(name = "Swerve Pod Tester", group = "TeleOp")
+@Configurable
 public class SwervePodTester extends OpMode {
 
     private SwervePodSubsystem[] pods;
+    public static double flOffset = 325, brOffset = 320;
+    public static boolean offsetUpdate = false;
+
 
     @Override
     public void init() {
         SwervePodSubsystem fl = new SwervePodSubsystem(-156.0, 156.0, "fls", "flm", "flsai", hardwareMap); // Front Left
         SwervePodSubsystem br = new SwervePodSubsystem(156.0, -156.0, "brs", "brm", "brsai", hardwareMap); // Back Right
         pods = new SwervePodSubsystem[]{fl, br};
-        fl.setServoOffsetDeg(315-90);
-        br.setServoOffsetDeg(60-90);
+        fl.setServoOffsetDeg(flOffset);
+        br.setServoOffsetDeg(brOffset);
     }
 
     @Override
@@ -32,6 +37,11 @@ public class SwervePodTester extends OpMode {
         telemetry.addLine(pods[0].debugText());
         telemetry.addLine(pods[1].debugText());
         telemetry.update();
+
+        if (offsetUpdate) {
+            pods[0].setServoOffsetDeg(flOffset);
+            pods[1].setServoOffsetDeg(brOffset);
+        }
 
     }
 
