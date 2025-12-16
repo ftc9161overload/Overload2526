945c0c222dd4e330602f156e50f6eef849ced449
Ryan Hajj <ryanjhajj72709@gmail.com>
2025-12-16 12:12:14 -0600

    Add toggle and isRunning methods to IntakeSubsystem
    Introduced a 'running' state to IntakeSubsystem and added toggle() and isRunning() methods for easier control and state checking of the intake motor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MidLevel/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MidLevel/IntakeSubsystem.java
@@ -11,15 +11,31 @@ public class IntakeSubsystem implements Subsystem {
     private final DcMotorEx motor;
     public double motorSpeed = 0.5;
 
+    private boolean running = false;
+
     public IntakeSubsystem(String motorName, HardwareMap hMap) {
         motor = hMap.get(DcMotorEx.class, motorName);
     }
 
     public void run() {
+        running = true;
         motor.setPower(motorSpeed);
     }
 
     public void stop() {
+        running = false;
         motor.setPower(0);
+    }
+
+    public void toggle() {
+        if (running) {
+            stop();
+        } else {
+            run();
+        }
+    }
+
+    public boolean isRunning() {
+        return running;
     }
 }
