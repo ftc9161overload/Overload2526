885470f4ecf47ae7d64c7cc4f56a53594f877ef1
Ryan Hajj <ryanjhajj72709@gmail.com>
2025-12-21 00:31:50 -0600

    Reapply "Added more to launcher and teleOp"
    This reverts commit 00507a472e68c01aa46208d9e72d78ec5ec5093c.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/UpperLevel/LauncherSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/UpperLevel/LauncherSubsystem.java
@@ -54,7 +54,7 @@ public class LauncherSubsystem extends SubsystemGroup {
     );
 
     // Turns the flipper servo on if the rotary is in an exact enough position
-    private Command launch = new InstantCommand(() -> {
+    private Command servoOn = new InstantCommand(() -> {
         if(getIsInPosition()) {
             OuttakeFlipperSubsystem.INSTANCE.setOn.requires();
         }
@@ -65,13 +65,25 @@ public class LauncherSubsystem extends SubsystemGroup {
 
             setup,
             new Delay(servoTime),
-            launch,
+            servoOn,
             new Delay(servoTime),
             OuttakeFlipperSubsystem.INSTANCE.setOff.requires(),
             new Delay(servoTime),
             RotarySubsystem.INSTANCE.nextChamber.requires(OuttakeFlipperSubsystem.INSTANCE)
 
     ).named("Launch");
+
+    public Command Launch2 = new SequentialGroup(
+            Launch1,
+            Launch1
+    ).named("Launch2");
+
+    public Command Launch3 = new SequentialGroup(
+            Launch1,
+            Launch1,
+            Launch1,
+            RotarySubsystem.INSTANCE.setHalfChamberOn
+    ).named("Launch3");
 
 
     // Previous code
