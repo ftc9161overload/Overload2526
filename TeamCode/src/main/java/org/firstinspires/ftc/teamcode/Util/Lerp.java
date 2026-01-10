4b42cec2bd36a0b267210541445a686590eb2a77
Ryan Hajj <ryanjhajj72709@gmail.com>
2026-01-09 21:31:53 -0600

    Add easing functions to Lerp utility class
    Introduced an Ease enum and support for linear, ease-in, ease-out, and ease-in-out interpolation in the Lerp class. Added methods to set the easing type and updated the interpolation logic to apply the selected easing function.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Lerp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Lerp.java
@@ -1,8 +1,18 @@
 package org.firstinspires.ftc.teamcode.Util;
 
 public class Lerp {
-    public double startNum;
 
+    public enum Ease {
+        LINEAR,
+        EASE_IN,
+        EASE_OUT,
+
+        EASE_IN_OUT
+    }
+
+    public Ease ease = Ease.LINEAR;
+
+    public double startNum;
     public double currentNum;
     public double targetNum;
     public double time;
@@ -12,26 +22,48 @@ public class Lerp {
      * Constructor for Lerp.
      * @param startNum The starting number
      * @param targetNum The target number to reach at the end of the time param
-     * @param time The length of time (in seconds) to preform
+     * @param time The length of time (in seconds) to perform
      */
     public Lerp(double startNum, double targetNum, double time){
-        this.startNum = currentNum;
+        this.startNum = startNum;
         this.targetNum = targetNum;
         this.time = time;
+        this.currentNum = startNum;
     }
 
     public void reset() {
         timer.reset();
     }
 
+    public void setEase(Ease easeType) {
+        this.ease = easeType;
+    }
+
     public void setNewTarget(double targetNum, double time) {
         timer.reset();
+        this.startNum = currentNum;   // start from wherever we are
         this.targetNum = targetNum;
         this.time = time;
     }
 
+    private double applyEase(double t) {
+        switch (ease) {
+            case EASE_IN:
+                return t * t;
+            case EASE_OUT:
+                return 1 - (1 - t) * (1 - t);
+            case EASE_IN_OUT:
+                return t * t * (3 - 2 * t);
+            default:
+                return t;
+        }
+    }
+
     public double getNum() {
-        currentNum = startNum + (targetNum - startNum) * Math.min(Math.max(timer.getTimeSeconds() / time, 0), 1);
+        double t = timer.getTimeSeconds() / time;
+        t = Math.min(Math.max(t, 0), 1);  // clamp 0–1=
+        t = applyEase(t);
+        currentNum = startNum + (targetNum - startNum) * t;
         return currentNum;
     }
 }
