66fd30aea61dc4ffd4e704b592b773a460362a9c
Ryan Hajj <ryanjhajj72709@gmail.com>
2026-02-26 15:37:11 -0600

    The poses I made
    And I forgot to push :(

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Poses.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Poses.java
@@ -32,5 +32,27 @@ public class Poses {
     public static Pose2D spike3Start    = new Pose2D(45.0,  36.0, Math.toRadians(180));
     public static Pose2D spike3End    = new Pose2D(11.0,  36.0, Math.toRadians(180));
 
+    // -*- Common Starts -*-
+    // Far
+    public static Pose startBlueFar = new Pose(25.674, 129.967, Math.toRadians(143.5));
+    public static Pose startRedFar = new Pose(118.645, 129.967, Math.toRadians(36.5));
+    // Close
+    public static Pose startBlueClose = new Pose(56.133, 8, Math.toRadians(90));
+    public static Pose startRedClose = new Pose(87.867, 8, Math.toRadians(90));
+
+
+    // Goal
+    //public static Pose blueGoal = new Pose(16, Math.toRadians(132));
+    //public static Pose redGoal = new Pose(128, Math.toRadians(132));
+
+    // Base
+    public static Pose blueBase = new Pose(105.203, 33.319, Math.toRadians(90));
+    public static Pose redBase = new Pose(38.545, 33.319, Math.toRadians(90));
+
+    // Gate
+    public static Pose gateBluePre = new Pose(20.571, Math.toRadians(71.123));
+    public static Pose gateBlue = new Pose(15.628, Math.toRadians(71.123));
+    public static Pose gateRedPre = new Pose(125.502, Math.toRadians(71.123));
+    public static Pose gateRed = new Pose(129.169, Math.toRadians(71.123));
 
 }
